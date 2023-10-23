6282702956a8a8897e44ad2c16eab8b45dca3fa0
Shirley Gao <shirleygao1112@gmail.com>
2023-10-23 18:44:56 +0800

    @Value读取项目配置的端口号

--- spzx-cloud-user/src/main/java/com/atguigu/spzx/cloud/service/impl/UserServiceImpl.java
+++ spzx-cloud-user/src/main/java/com/atguigu/spzx/cloud/service/impl/UserServiceImpl.java
@@ -4,6 +4,7 @@ import com.atguigu.spzx.cloud.entity.User;
 import com.atguigu.spzx.cloud.mapper.UserMapper;
 import com.atguigu.spzx.cloud.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 /**
@@ -18,8 +19,12 @@ import org.springframework.stereotype.Service;
 public class UserServiceImpl implements UserService {
     @Autowired
     private UserMapper userMapper;
+    @Value("${server.port}") ///读取项目中的server.port配置的值，注入String port
+    private String port;
     @Override
     public User findById(long id) {
-        return userMapper.findById(id);
+        User user = userMapper.findById(id);
+        user.setUserName(user.getUserName()+ ",端口号:" +port);
+        return user;
     }
 }
