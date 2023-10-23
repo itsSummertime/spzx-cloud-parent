1df2f91a93c26fd1fb07c1ad1bceb8a4c97c70dd
Shirley Gao <shirleygao1112@gmail.com>
2023-10-23 19:34:32 +0800

    服务远程调用-@LoadBalanced

--- spzx-cloud-order/src/main/java/com/atguigu/spzx/cloud/config/RestConfig.java
+++ spzx-cloud-order/src/main/java/com/atguigu/spzx/cloud/config/RestConfig.java
@@ -1,5 +1,6 @@
 package com.atguigu.spzx.cloud.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -8,6 +9,7 @@ import org.springframework.web.client.RestTemplate;
 public class RestConfig {
 
     @Bean //将方法的返回值存入Spring容器
+    @LoadBalanced //启用负载均衡的功能，可以通过服务名远程调用
     public RestTemplate getRestTemplate(){
         return new RestTemplate();
     }
--- spzx-cloud-order/src/main/java/com/atguigu/spzx/cloud/service/impl/OrderServiceImpl.java
+++ spzx-cloud-order/src/main/java/com/atguigu/spzx/cloud/service/impl/OrderServiceImpl.java
@@ -5,13 +5,9 @@ import com.atguigu.spzx.cloud.entity.User;
 import com.atguigu.spzx.cloud.mapper.OrderMapper;
 import com.atguigu.spzx.cloud.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.List;
-import java.util.Random;
 
 /**
  * <p>
@@ -35,36 +31,21 @@ public class OrderServiceImpl implements OrderService {
     private RestTemplate restTemplate;
     @Autowired
     private DiscoveryClient discoveryClient;
+
+
     @Override
     public Order findById(long id) {
         //根据订单id查询到 订单信息
         Order order = orderMapper.findById(id);
         //获取到用户id
         Long userId = order.getUserId();
-        //根据服务名 通过DiscoveryClient对象动态获取一个服务的实例(微服务的调用地址)
-        ServiceInstance serviceInstance = this.getserviceInstance("spzx-cloud-user");
-        //获取服务的ip地址
-        String host = serviceInstance.getHost();
-        //获取服务的端口号
-        int port = serviceInstance.getPort();
 
         //>>>远程调用 用户服务 获取用户信息
-        //发送Get请求  参数1:请求的地址   参数2:接收返回值的类
-        User user = restTemplate.getForObject("http://"+host+":"+port+"/user/findById/"+userId, User.class);
+        //如果使用服务名调用，则必须加上@LoadBalanced，默认使用轮询的负载均衡算法
+        User user = restTemplate.getForObject("http://spzx-cloud-user/user/findById/"+userId, User.class);
 
         //封装到order对象中后返回
         order.setUser(user);
         return order;
-    }
-    //根据服务名获取服务实列
-    public ServiceInstance getserviceInstance(String name){
-        //获取服务列表
-        List<ServiceInstance> list = discoveryClient.getInstances(name)         ;
-
-        //随机生成一个下标
-        int random = new Random().nextInt(list.size());
-        //随机返回一个服务实例 负载均衡的算法
-        return list.get(random);
-
     }
 }
